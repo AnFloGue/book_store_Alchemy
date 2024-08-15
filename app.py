668dd772d78fe44f22a4805d6daf4c00c5ccbb11
ANflo <75026239+AnFloGue@users.noreply.github.com>
2024-08-15 15:26:11 +0200

    03 Commit:

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from data_models import db, Author, Book
 import os
+import requests
 
 database_dir = os.path.join(os.getcwd(), 'data')
 if not os.path.exists(database_dir):
@@ -28,13 +29,24 @@ def get_book_cover(isbn):
 @app.route('/', methods=['GET'])
 def home():
     sort_by = request.args.get('sort_by', 'title')
-    if sort_by == 'author':
-        books = Book.query.join(Author).order_by(Author.name).all()
+    search_query = request.args.get('search_query', '')
+
+    if search_query:
+        books = Book.query.join(Author).filter(
+            (Book.title.ilike(f'%{search_query}%')) |
+            (Author.name.ilike(f'%{search_query}%'))
+        ).all()
     else:
-        books = Book.query.order_by(Book.title).all()
+        if sort_by == 'author':
+            books = Book.query.join(Author).order_by(Author.name).all()
+        else:
+            books = Book.query.order_by(Book.title).all()
+
     for book in books:
         book.cover_url = get_book_cover(book.isbn)
+    
     return render_template('home.html', books=books)
+
 
 @app.route('/add_author', methods=['GET', 'POST'])
 def add_author():
@@ -59,6 +71,23 @@ def add_book():
         flash('Book added successfully!')
         return redirect(url_for('add_book'))
     return render_template('add_book.html', authors=authors)
+
+@app.route('/book/<int:book_id>/delete', methods=['POST'])
+def delete_book(book_id):
+    book = Book.query.get_or_404(book_id)
+    author_id = book.author_id
+    db.session.delete(book)
+    db.session.commit()
+
+    # Check if the author has any other books
+    author_books = Book.query.filter_by(author_id=author_id).count()
+    if author_books == 0:
+        author = Author.query.get(author_id)
+        db.session.delete(author)
+        db.session.commit()
+
+    flash('Book deleted successfully!')
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
